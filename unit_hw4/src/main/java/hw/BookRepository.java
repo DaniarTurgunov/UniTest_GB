5575d9af49b43dda7093304476bc6c114fb709a2
DaniarTurgunov <114252591+DaniarTurgunov@users.noreply.github.com>
2023-10-14 04:20:20 +0500

    upload hw4

---
+++ unit_hw4/src/main/java/hw/BookRepository.java
@@ -0,0 +1,8 @@
+package hw;
+
+import java.util.List;
+
+public interface BookRepository {
+    Book findById(String id);
+    List<Book> findAll();
+}
---
+++ unit_hw4/src/test/java/BookServiceTest.java
@@ -0,0 +1,26 @@
+import hw.Book;
+import hw.BookService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookServiceTest {
+    BookService bookService = Mockito.mock(BookService.class);
+    Book book1 = new Book("1", "Book1", "Author1");
+    Book book2 = new Book("2", "Book2", "Author2");
+
+    @Test
+    void findBookById() {
+        Mockito.when(bookService.findBookById("1")).thenReturn(book1);
+    }
+
+    @Test
+    void findAllBooks() {
+        List<Book> allBooks = new ArrayList<Book>();
+        allBooks.add(book1);
+        allBooks.add(book2);
+        Mockito.when(bookService.findAllBooks()).thenReturn(allBooks);
+    }
+}
